b522d11e0bdf0710ab4a3fd6eb30ffcb9e15f3a7
unknown <sergioarevalo301@gmail.com>
2024-05-28 21:25:44 -0500

    feat:/app/components/nacvbar

---
+++ app/components/UI/navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Box, Typography } from "@mui/material";
+import logo from "../../../public/brand.webp";
+import Image from "next/image";
+import { getFromLocalStorage } from "@/app/_Utilities/localStorage";
+import { useEffect, useState } from "react";
+import { UserCredential } from "firebase/auth";
+
+const Navbar = () => {
+  const [user, setUser] = useState<UserCredential | null>(null);
+  useEffect(() => {
+    const localUser = getFromLocalStorage('user');
+    console.log(localUser)
+    setUser(localUser);
+  }, []);
+  return (
+    <Box
+      sx={{
+        margin: {xs:"5px 10px", md:"5px 20px 10px 20px"},
+        borderBottom: { xs: "1px solid grey", md: "none" },
+        display:"flex",
+        justifyContent:"space-between",
+        alignItems:"center"
+      }}
+    >
+      <Image
+        src={logo}
+        alt="Brand Logo"
+        width={170}
+        height={50}
+      />
+      {user && <Typography variant="h4" sx={{fontSize:{xs:14, md:20 }, textAlign:"right"}}>Administrador {user.user.displayName || user.user.email}</Typography>}
+    </Box>
+  );
+};
+
+export default Navbar;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,20 @@
 import { Box } from "@mui/material";
 
+import { Metadata } from "next";
+import Navbar from "../components/UI/navbar";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Manage your products",
+};
+
 const Dashboard = () => {
-    return ( 
-        <Box>Estamos en el dashboard</Box>
-     );
-}
- 
+  return (
+    <Box>
+      <Navbar />
+      Estamos en el dashboard
+    </Box>
+  );
+};
+
 export default Dashboard;
