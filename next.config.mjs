45eb2d189d88c4a0d68829ec12c6d042e3d14bfd
unknown <sergioarevalo301@gmail.com>
2024-05-29 20:45:02 -0500

    test deploy fix route register

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['www.cloudlabs.us', 'cdn.dummyjson.com', 'firebasestorage.googleapis.com'],
       },
+      trailingSlash: true,
 };
 
 export default nextConfig;
